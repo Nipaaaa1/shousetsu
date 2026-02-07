bcf1cd12ce70916f16cee18654cc323e131f4b7c
Nipaaaa1 <frostone224@gmail.com>
2026-02-07 10:03:33 +0000

    feat:add latest arc in home page

--- home/models.py
+++ home/models.py
@@ -1,9 +1,18 @@
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
+from novel.models import ArcDetailPage
 
 
 class HomePage(Page):
     description = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [FieldPanel("description")]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        latest_arc = ArcDetailPage.objects.live().order_by("-arc_number").first()  # pyright: ignore
+
+        context["latest_arc"] = latest_arc
+
+        return context
