35b530f96c4a897943694e35e1fcddda656a6521
Nipaaaa1 <frostone224@gmail.com>
2026-02-15 11:29:01 +0000

    feat: add socias links

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,7 +1,24 @@
 from django.db import models
+from wagtail import blocks
 from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
+from wagtail.fields import StreamField
+
+
+class SocialBlock(blocks.StructBlock):
+    provider = blocks.ChoiceBlock(
+        choices=[
+            ("facebook", "Facebook"),
+            ("x", "X"),
+            ("instagram", "Instagram"),
+            ("threads", "Threads"),
+        ]
+    )
+    url = blocks.URLBlock()
 
 
 @register_setting
 class FooterSettings(BaseGenericSetting):
     description = models.TextField(default="Novel App Shousetsu")
+    social_media = StreamField(
+        [("social", SocialBlock())], blank=True, use_json_field=True
+    )
